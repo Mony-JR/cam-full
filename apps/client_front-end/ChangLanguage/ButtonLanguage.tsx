cf5bb28b87f69d5d6da98e8e35faad98d19b1284
MonyJR <mony@MacBook-Air.local>
2024-06-14 21:10:08 +0700

    beta release@3.1.0

--- apps/client_front-end/ChangLanguage/ButtonLanguage.tsx
+++ apps/client_front-end/ChangLanguage/ButtonLanguage.tsx
@@ -8,7 +8,7 @@ const ButtonLanguage:React.FC = () => {
     const [swich,setSwich]=useState<boolean>(false)
 
   return (
-    <div onClick={()=>setSwich(!swich)} className={`flex w-10 h-5 rounded-2xl ${swich? 'bg-white': ' bg-blue-300 '} relative `}>
+    <div onClick={()=>setSwich(!swich)} className={`flex w-10 h-5 rounded-2xl bg-white relative `}>
 
         <span className={` absolute   ${swich? ' left-1' : ' right-0' } `} > <Image src={swich? kh:eng} alt={""} width={20}  /> </span>
 
